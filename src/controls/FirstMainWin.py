dc3c0924c7f65c7a2f6bf7d8155f1a305df55d2e
liang412 <ljy1979400981@gmail.com>
2022-04-26 21:07:51 +0800

    feat(main):init project of upload-to-github

--- src/controls/FirstMainWin.py
+++ src/controls/FirstMainWin.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow,QApplication
 from PyQt5.QtGui import QIcon
+#  QIcon用于添加图标
 
 class FirstMainWin(QMainWindow):
     def __init__(self):
@@ -12,12 +13,13 @@ class FirstMainWin(QMainWindow):
         # 设置窗口的尺寸
         self.resize(400,300)
 
+        # 添加状态栏
         self.status = self.statusBar()
 
         self.status.showMessage('只存在5秒的消息',5000)
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QApplication(sys.argv)  # 传入参数
 
     app.setWindowIcon(QIcon('./images/Dragon.ico'))
     main = FirstMainWin()
